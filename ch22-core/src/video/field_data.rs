56e00d9e46c965755d81ebf98a40b591c117a517
jonelantha <57597570+jonelantha@users.noreply.github.com>
2025-11-26 19:04:24 +0000

    refactor snapshot_scanline into paths for teletext/hires

--- ch22-core/src/video/field_data.rs
+++ ch22-core/src/video/field_data.rs
@@ -22,57 +22,98 @@ impl Field {
         }
     }
 
-    pub fn snapshot_scanline<'a, F>(
+    pub fn snapshot_scanline<'a>(
         &mut self,
         line_index: usize,
         crtc_memory_address: u16,
         crtc_raster_address_even: u8,
         ic32_latch: u8,
         video_registers: &VideoRegisters,
-        get_buffer: F,
-    ) where
-        F: Fn(std::ops::Range<u16>) -> &'a [u8],
-    {
+        get_buffer: impl Fn(std::ops::Range<u16>) -> &'a [u8],
+    ) {
         self.lines[line_index].set_registers(
             crtc_memory_address,
             crtc_raster_address_even,
             video_registers,
         );
 
-        let crtc_length = video_registers.crtc_r1_horizontal_displayed;
-        let ula_teletext = video_registers.is_ula_teletext();
-
-        if crtc_length == 0
-            || (!ula_teletext && crtc_raster_address_even >= 8)
-            || (!ula_teletext && video_registers.is_crtc_screen_delay_no_output())
-        {
-            self.lines[line_index].set_blank();
-            return;
-        }
-
-        if (ula_teletext
-            && !VideoMemoryAccess::is_crtc_range_telextext(crtc_memory_address, crtc_length))
-            || (!ula_teletext
-                && !VideoMemoryAccess::is_crtc_range_hires(crtc_memory_address, crtc_length))
-        {
-            self.lines[line_index].set_invalid();
-            return;
-        }
-
-        let (first_ram_range, second_ram_range) =
-            VideoMemoryAccess::translate_crtc_range(crtc_memory_address, crtc_length, ic32_latch);
-
-        let first_ram_slice = get_buffer(first_ram_range);
-        let second_ram_slice = second_ram_range.map(get_buffer);
-
-        if ula_teletext {
-            self.lines[line_index].set_char_data(first_ram_slice, second_ram_slice);
+        if video_registers.is_ula_teletext() {
+            snapshot_teletext_scanline_data(
+                &mut self.lines[line_index],
+                crtc_memory_address,
+                ic32_latch,
+                video_registers,
+                get_buffer,
+            );
         } else {
-            self.lines[line_index].set_char_data_for_raster(
-                first_ram_slice,
-                second_ram_slice,
+            snapshot_hires_scanline_raster_data(
+                &mut self.lines[line_index],
+                crtc_memory_address,
                 crtc_raster_address_even,
+                ic32_latch,
+                video_registers,
+                get_buffer,
             );
         }
     }
+}
+
+fn snapshot_teletext_scanline_data<'a>(
+    field_line: &mut FieldLine,
+    crtc_memory_address: u16,
+    ic32_latch: u8,
+    video_registers: &VideoRegisters,
+    get_buffer: impl Fn(std::ops::Range<u16>) -> &'a [u8],
+) {
+    let crtc_length = video_registers.crtc_r1_horizontal_displayed;
+    if crtc_length == 0 {
+        field_line.set_blank();
+        return;
+    }
+
+    if !VideoMemoryAccess::is_crtc_range_telextext(crtc_memory_address, crtc_length) {
+        field_line.set_invalid();
+        return;
+    }
+
+    let (first_ram_range, second_ram_range) =
+        VideoMemoryAccess::translate_crtc_range(crtc_memory_address, crtc_length, ic32_latch);
+
+    field_line.set_char_data(
+        get_buffer(first_ram_range),
+        second_ram_range.map(get_buffer),
+    );
+}
+
+fn snapshot_hires_scanline_raster_data<'a>(
+    field_line: &mut FieldLine,
+    crtc_memory_address: u16,
+    crtc_raster_address_even: u8,
+    ic32_latch: u8,
+    video_registers: &VideoRegisters,
+    get_buffer: impl Fn(std::ops::Range<u16>) -> &'a [u8],
+) {
+    let crtc_length = video_registers.crtc_r1_horizontal_displayed;
+
+    if crtc_length == 0
+        || crtc_raster_address_even >= 8
+        || video_registers.is_crtc_screen_delay_no_output()
+    {
+        field_line.set_blank();
+        return;
+    }
+
+    if !VideoMemoryAccess::is_crtc_range_hires(crtc_memory_address, crtc_length) {
+        field_line.set_invalid();
+        return;
+    }
+
+    let (first_ram_range, second_ram_range) =
+        VideoMemoryAccess::translate_crtc_range(crtc_memory_address, crtc_length, ic32_latch);
+
+    field_line.set_char_data_for_raster(
+        get_buffer(first_ram_range),
+        second_ram_range.map(get_buffer),
+        crtc_raster_address_even,
+    );
 }
