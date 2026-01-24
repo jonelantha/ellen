dd6229ed8b93ec546e1edf6d693b8d39ecc8e088
jonelantha <57597570+jonelantha@users.noreply.github.com>
2026-01-24 09:02:25 +0000

    fixture refactor

--- render/tests/field-data-renderer.spec.ts
+++ render/tests/field-data-renderer.spec.ts
@@ -1,23 +1,22 @@
 import { test, expect } from '@playwright/test';
-import { globSync, readFileSync } from 'node:fs';
+import { glob, readFile } from 'node:fs/promises';
 import { basename } from 'node:path';
 
-const fieldDataFixtures = globSync('field-data-fixtures/*.bin').map(file => ({
-  name: basename(file, '.bin'),
-  fieldData: new Uint8Array(readFileSync(file)),
-}));
+for await (const fieldDataFixture of glob('field-data-fixtures/*.bin')) {
+  const fixtureName = basename(fieldDataFixture, '.bin');
 
-for (const { name, fieldData } of fieldDataFixtures) {
-  test.describe.parallel(name, () => {
+  test.describe.parallel(fixtureName, () => {
     test('renders correctly', async ({ page }) => {
+      const fieldData = new Uint8Array(await readFile(fieldDataFixture));
+
       await page.goto('/');
 
       await page.evaluate(fieldData => window.testRender(fieldData), fieldData);
 
-      await expect(page.locator('canvas')).toHaveScreenshot(`${name}.png`, {
-        maxDiffPixels: 100,
-        threshold: 0.2,
-      });
+      await expect(page.locator('canvas')).toHaveScreenshot(
+        `${fixtureName}.png`,
+        { maxDiffPixels: 100, threshold: 0.2 },
+      );
     });
   });
 }
