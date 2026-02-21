5b438ac28e6f927a9cfa41c73d647327eda87197
jonelantha <57597570+jonelantha@users.noreply.github.com>
2026-02-21 16:54:18 +0000

    char_row_control -> char_raster_control

--- ch22-core/src/video/crtc.rs
+++ ch22-core/src/video/crtc.rs
@@ -4,12 +4,12 @@ use crate::video::VideoRegisters;
 
 mod address_control;
 mod beam_control;
-mod char_row;
+mod char_raster_control;
 mod vsync_control;
 
 use self::address_control::AddressControl;
 use self::beam_control::BeamControl;
-use self::char_row::{CharRowControl, CharRowPosition};
+use self::char_raster_control::{CharRasterControl, CharRasterPosition};
 use self::vsync_control::VSyncControl;
 
 #[cfg(test)]
@@ -25,7 +25,7 @@ pub struct SnapshotParams {
 
 #[derive(Default)]
 pub struct Crtc {
-    char_row_control: CharRowControl,
+    char_raster_control: CharRasterControl,
     address_control: AddressControl,
     vsync_control: VSyncControl,
     beam_control: BeamControl,
@@ -34,7 +34,7 @@ pub struct Crtc {
 
 impl Crtc {
     pub fn init(&mut self, registers: &VideoRegisters) {
-        self.char_row_control.reset();
+        self.char_raster_control.reset();
         self.address_control.reset(registers);
         self.beam_control.reset();
         self.vsync_control.reset();
@@ -46,18 +46,18 @@ impl Crtc {
 
         self.vsync_control.advance_scanline();
 
-        self.char_row_control.advance_scanline(registers);
+        self.char_raster_control.advance_scanline(registers);
 
-        if let Some(position) = self.char_row_control.get_position(registers) {
+        if let Some(position) = self.char_raster_control.get_position(registers) {
             match position {
-                CharRowPosition::StartOfField => {
+                CharRasterPosition::StartOfField => {
                     self.address_control.reset(registers);
                     self.odd_field = !self.odd_field;
                 }
-                CharRowPosition::StartOfCharRow => {
+                CharRasterPosition::StartOfCharRow => {
                     self.address_control.advance_char_row(registers);
                 }
-                CharRowPosition::VsyncStart => {
+                CharRasterPosition::VsyncStart => {
                     if self.vsync_control.start_vsync_period(registers) {
                         self.beam_control.reset();
                     }
@@ -76,11 +76,11 @@ impl Crtc {
 
     pub fn get_snapshot_params(&self, registers: &VideoRegisters) -> SnapshotParams {
         SnapshotParams {
-            in_scan: self.char_row_control.is_in_scan(registers),
+            in_scan: self.char_raster_control.is_in_scan(registers),
             beam_scanline: self.beam_control.get_scanline(),
             address: self.address_control.get_address(),
-            raster_address_even: self.char_row_control.get_raster_address_even(registers),
-            raster_address_odd: self.char_row_control.get_raster_address_odd(registers),
+            raster_address_even: self.char_raster_control.get_raster_address_even(registers),
+            raster_address_odd: self.char_raster_control.get_raster_address_odd(registers),
         }
     }
 
@@ -94,7 +94,7 @@ impl Crtc {
         // in interlace mode, each field is offset by half a raster
         // so a delay for the two halves is inserted every other field
         // (this is an approximation)
-        if self.char_row_control.is_at_start() && self.odd_field && registers.r8_is_interlace() {
+        if self.char_raster_control.is_at_start() && self.odd_field && registers.r8_is_interlace() {
             next_scanline_trigger *= 2;
         }
 
--- ch22-core/src/video/crtc/char_raster_control.rs
+++ ch22-core/src/video/crtc/char_raster_control.rs
@@ -1,60 +1,60 @@
 use crate::video::VideoRegisters;
 
-pub enum CharRowPosition {
+pub enum CharRasterPosition {
     StartOfField,
     StartOfCharRow,
     VsyncStart,
 }
 
 #[derive(Default)]
-pub struct CharRowControl {
+pub struct CharRasterControl {
     char_row: u8,
-    char_raster: u8,
+    char_raster_in_row: u8,
 }
 
-impl CharRowControl {
+impl CharRasterControl {
     pub fn reset(&mut self) {
         self.char_row = 0;
-        self.char_raster = 0;
+        self.char_raster_in_row = 0;
     }
 
     pub fn advance_scanline(&mut self, registers: &VideoRegisters) {
         debug_assert!(registers.crtc_r4_vertical_total <= 0x7F); // r4 should only be 7 bit
         debug_assert!(registers.crtc_r5_vertical_total_adjust <= 0x1F); // r5 should only be 5 bit
 
-        self.char_raster += 1;
+        self.char_raster_in_row += 1;
 
         if self.char_row <= registers.crtc_r4_vertical_total
-            && self.char_raster >= registers.r8_r9_rasters_per_char()
+            && self.char_raster_in_row >= registers.r8_r9_rasters_per_char()
         {
             self.char_row += 1;
-            self.char_raster = 0;
+            self.char_raster_in_row = 0;
         }
 
         if self.char_row > registers.crtc_r4_vertical_total
-            && self.char_raster >= registers.crtc_r5_vertical_total_adjust
+            && self.char_raster_in_row >= registers.crtc_r5_vertical_total_adjust
         {
             self.reset();
         }
     }
 
-    pub fn get_position(&self, registers: &VideoRegisters) -> Option<CharRowPosition> {
-        if self.char_row == 0 && self.char_raster == 0 {
-            Some(CharRowPosition::StartOfField)
-        } else if self.char_raster == 0 {
-            Some(CharRowPosition::StartOfCharRow)
+    pub fn get_position(&self, registers: &VideoRegisters) -> Option<CharRasterPosition> {
+        if self.char_row == 0 && self.char_raster_in_row == 0 {
+            Some(CharRasterPosition::StartOfField)
+        } else if self.char_raster_in_row == 0 {
+            Some(CharRasterPosition::StartOfCharRow)
         } else if self.char_row == registers.crtc_r7_vertical_sync_position
             // after first raster of vsync pos
-            && self.char_raster == 1
+            && self.char_raster_in_row == 1
         {
-            Some(CharRowPosition::VsyncStart)
+            Some(CharRasterPosition::VsyncStart)
         } else {
             None
         }
     }
 
     pub fn is_at_start(&self) -> bool {
-        self.char_row == 0 && self.char_raster == 0
+        self.char_row == 0 && self.char_raster_in_row == 0
     }
 
     pub fn is_in_scan(&self, registers: &VideoRegisters) -> bool {
@@ -63,9 +63,9 @@ impl CharRowControl {
 
     pub fn get_raster_address_even(&self, registers: &VideoRegisters) -> u8 {
         if registers.r8_is_interlace_sync_and_video() {
-            self.char_raster << 1
+            self.char_raster_in_row << 1
         } else {
-            self.char_raster
+            self.char_raster_in_row
         }
     }
 
